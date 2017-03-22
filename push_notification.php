f745334c76f9f1db1263f005e40b0cb55dfa3452
Xand'r VanBurke <xandercb@gmail.com>
2017-03-21 19:45:59 -0500

    Update push_notification.php

--- push_notification.php
+++ push_notification.php
@@ -1,36 +1,33 @@
 <?php 
-		Global $tokens;
-		Global $message;
+	Global $tokens;
+	Global $message;
 	
-	function send_notification ($tokens, $message)
-	{
-		
-		
+	function send_notification ($tokens, $message) {		
 		$url = 'https://fcm.googleapis.com/fcm/send';
 		$fields = array(
-			 'registration_ids' => $tokens,
-			 'data' => $message
-			);
+			'registration_ids' => $tokens,
+			'data' => $message
+		);
 
 		$headers = array(
 			'Authorization:key = AIzaSyD63Pi-Qw6pQVMou6j5pnDNBHOeaaxx8iE ',
 			'Content-Type: application/json'
-			);
+		);
 
-	   $ch = curl_init();
-       curl_setopt($ch, CURLOPT_URL, $url);
-       curl_setopt($ch, CURLOPT_POST, true);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
-       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-       $result = curl_exec($ch);           
-       if ($result === FALSE) {
-           die('Curl failed: ' . curl_error($ch));
-       }
-       curl_close($ch);
-       return $result;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);           
+			if ($result === FALSE) {
+				die('Curl failed: ' . curl_error($ch));
+			}
+			curl_close($ch);
+       	return $result;
 	}
 
  ?>

e1e52c8553b0daca22e788ff8070e8948b862252
Xand'r VanBurke <xandercb@gmail.com>
2017-03-21 19:41:12 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,11 +1,10 @@
 <?php 
 	if (!empty($_POST["Token"])) {		
 		$_uv_Token=$_POST["Token"];
-	   	$conn = mysqli_connect("localhost","firebaseUser","Gallardo24","hc_firebase") or die("Error connecting");
+	   	$conn = mysqli_connect("localhost","db-name","db-password","table-name") or die("Error connecting");
 		$q="INSERT INTO users (Token) VALUES ( '$_uv_Token') "
               	." ON DUPLICATE KEY UPDATE Token = '$_uv_Token';";              
                 mysqli_query($conn,$q) or die(mysqli_error($conn));
         mysqli_close($conn);
-
 	} 
 ?>
